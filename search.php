c2e5c75a495ef07e08116faab37cac513f319a7a
Henry Mound <henrymound@gmail.com>
2015-02-24 01:50:26 +0000

    changed search files

--- search.php
+++ search.php
@@ -1,7 +1,25 @@
 <html>
 <body>
 
-        You searched for: <?php echo $_POST["searchTerm"]; ?>
+        <?php
+        
+        $searchTerm = $_POST["searchTerm"];
+        $searchTerm = str_replace(' ', '+', $searchTerm);
+        echo $searchTerm;
+        $searchTerm = 'https://www.google.com/search?q='.$searchTerm.'&ie=utf-8&oe=utf-8';
+        
+        $pageResults = htmlentities(file_get_contents($searchTerm));
+        $doc = new DOMDocument();
+                @$doc->loadHTML(file_get_contents($searchTerm));
 
+                $tags = $doc->getElementsByTagName('mnr-c');
+
+        foreach ($tags as $tag) {
+       echo $tag->gettext();
+        foreach($pageResults->find("<b>") as $row) {
+             echo $row;
+        }
+        
+        ?>
 </body>
 </html> 
