f7f7a51d8d151e0a5d109fa089796535a3f29f23
henrymound <henrymound@gmail.com>
2015-02-23 20:46:19 -0500

    Created iPhone app basics and added chartjs library

---
+++ WikiGraphics/WikiGraphics/ViewController.h
@@ -0,0 +1,30 @@
+//
+//  ViewController.h
+//  WikiGraphics
+//
+//  Created by Henry O Mound on 2/23/15.
+//  Copyright (c) 2015 Henry Mound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Libraries/Classes/JBChartView/JBChartView.h"
+#import "Libraries/Classes/JBChartView/JBBarChartView.h"
+#import "Libraries/Classes/JBChartView/JBLineChartView.h"
+
+@interface ViewController : UIViewController{
+    
+    IBOutlet UIButton *NewGraphButton;
+    IBOutlet UIButton *AddWidget;
+    
+    IBOutlet UITextField *NewGraphTitle;
+
+}
+
+@property (strong, nonatomic) IBOutlet UIButton *NewGraphButton;
+@property (strong, nonatomic) IBOutlet UIButton *AddWidget;
+
+@property (strong, nonatomic) IBOutlet UITextField *NewGraphTitle;
+
+
+@end
+
